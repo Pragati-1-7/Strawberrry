13167336ad097edc3c8b6f155863f4e67261595d
Pragati-1-7 <pragati.lunkad241@vit.edu>
2026-05-02 12:31:05 +0530

    deployment changes

--- src/app/order/OrderForm.tsx
+++ src/app/order/OrderForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import Image from "next/image";
+
 
 export default function OrderForm({ userEmail }: { userEmail: string | null }) {
   const [quantity, setQuantity] = useState<number>(1);
